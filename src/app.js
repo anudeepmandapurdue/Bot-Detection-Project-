faf6697c7c78550406e35f2ccc9ea0197c32591f
anudeepmandapurdue <anudeepsm13@gmail.com>
2026-01-04 15:06:15 -0500

    updated code to have different files, logic not changed but allows for a seperate evulate method to use.

--- src/app.js
+++ src/app.js
@@ -2,20 +2,25 @@
 const express = require("express");
 const healthRouter = require("./routes/health");
 const eventRouter = require("./routes/event");
+const evaluateRouter = require("./routes/evaluate");
+
 
 function createApp(){
     const app = express();
 
     app.use(express.json());
+    app.locals.ipEvents = new Map();
+    app.locals.MAX_EVENTS_PER_IP = 200;
     app.use("/health", healthRouter);
     app.use("/v1/event", eventRouter);
+    app.use("/v1/evaluate", evaluateRouter);
     app.use((req, res)=>{
         res.status(404).json({error: "Not Found"})
     });
     app.use((err, req, res, next) => {
-    console.error("Unhandled error:", err);
-    res.status(500).json({ error: "Internal Server Error" });
-  });
+      console.error("Unhandled error:", err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
 
   return app;
 }
